c72a61ed62001ec265dead88ba7afd4bd52a5e77
Chaker Amine <chkramine2003@gmail.com>
2024-06-10 12:08:08 +0100

    finished dt exos

--- algoDs/exercices.py
+++ algoDs/exercices.py
@@ -1,11 +1,26 @@
-### Make a number given negative
+### DATA TYPES 
+## Make a number given negative
 def make_negative( number ):
     if number > 0:
         return -number
     else :
         return number
 make_negative(-12)
+
 # the abs function gives the absolute value of a number
 def makeNegative(num):
     return -abs(num)
 makeNegative(42)
+
+## remove the first and last charachter from string
+def remove_char(s):
+    return s[1:-1]
+
+## remove spaces in the string 
+def noSpace(x):
+    word = ""
+    for char in x:
+        if char != " " :
+            word = word + char
+    return word
+noSpace("Hello wolrd")
