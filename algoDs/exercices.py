78df21bee9612240e4286d352e2e787eb20d8726
Chaker Amine <chkramine2003@gmail.com>
2024-06-10 11:24:40 +0100

    started algo and ds

---
+++ algoDs/exercices.py
@@ -0,0 +1,11 @@
+### Make a number given negative
+def make_negative( number ):
+    if number > 0:
+        return -number
+    else :
+        return number
+make_negative(-12)
+# the abs function gives the absolute value of a number
+def makeNegative(num):
+    return -abs(num)
+makeNegative(42)
